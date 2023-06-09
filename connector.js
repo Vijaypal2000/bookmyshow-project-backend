8f13f804b15bd507675b6058ce79a97cb6d156c6
Vijaypal2000 <118725551+Vijaypal2000@users.noreply.github.com>
2023-06-09 15:22:57 +0530

    Initial commit

---
+++ connector.js
@@ -0,0 +1,17 @@
+let mongoose = require("mongoose");
+require("dotenv").config();
+const mongoURI = process.env.MONGOOSE_URI || "mongodb://localhost:27017/booking";
+
+const connectToDB = async () =>{
+     
+    mongoose
+        .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("connection established with mongodb server online");
+        })
+        .catch((err) => {
+            console.log("error while connection", err);
+        });
+}
+
+module.exports = connectToDB;
---
+++ route.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const bookMovieSchema = require("./schema");
+
+// Creating new booking and adding it to database.
+router.post("/booking", async (req, res) => {
+	const myBooking = new bookMovieSchema({
+		movie: req.body.movie,
+		slot: req.body.slot,
+		seats: req.body.seats,
+	});
+	const savedBooking = await myBooking.save();
+
+	if (savedBooking) {
+		//on successfull
+		res
+			.status(200)
+			.json({ data: savedBooking, message: "Booking successful!" });
+	} else {
+		//on error
+		res.status(500).json({
+			data: null,
+			message: "Something went wrong!. Please try again.",
+		});
+	}
+});
+
+// Getting last booking details form database and sending it to frontend.
+
+router.get("/booking", async (req, res) => {
+	const myBooking = await bookMovieSchema.find().sort({ _id: -1 }).limit(1);
+
+	if (myBooking.length === 0) {
+		//Sending "No previous Booking found" message if no last booking found
+		res.status(200).json({ data: null, message: "No previous Booking found!" });
+	} else {
+		//Sending data if last booking found
+		res.status(200).json({ data: myBooking[0] });
+	}
+});
+
+module.exports = router;
